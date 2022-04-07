10a0c37339cc5d450810ee8b76fce689d2e8ffca
Samy <samypulat@gmail.com>
2022-04-07 12:03:56 +0200

    test projet

---
+++ resources/views/back/partials/navbar.blade.php
@@ -0,0 +1,30 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="d-flex justify-content-between">
+                <div class="logo">
+                    <a href="{{ route("back.index") }}">Relations-Exercice-3</a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item {{ request()->routeIs("back.index") ? "active" : "" }}">
+                    <a href="{{ route("back.index") }}" class="sidebar-link">
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-title">Page</li>
+
+            </ul>  {{-- anchor_navbar --}}
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
