387f57d9960d011c3dcca83442469b250c095b66
Samy <samypulat@gmail.com>
2022-04-08 09:06:46 +0200

    plop

--- resources/views/front/partials/header.blade.php
+++ resources/views/front/partials/header.blade.php
@@ -1,12 +1,12 @@
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky">
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <nav class="main-nav">
                     <!-- ***** Logo Start ***** -->
                     <a href="index.html" class="logo">
-                        <img src="assets/images/templatemo-eduwell.png" alt="EduWell Template">
+                        <img src="{{ asset('images/templatemo-eduwell.png') }}" alt="EduWell Template">
                     </a>
                     <!-- ***** Logo End ***** -->
                     <!-- ***** Menu Start ***** -->
@@ -14,17 +14,19 @@
                         <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
                         <li class="scroll-to-section"><a href="#services">Services</a></li>
                         <li class="scroll-to-section"><a href="#courses">Courses</a></li>
-                        <li class="has-sub">
-                            <a href="javascript:void(0)">Pages</a>
-                            <ul class="sub-menu">
-                                <li><a href="about-us.html">About Us</a></li>
-                                <li><a href="our-services.html">Our Services</a></li>
-                                <li><a href="contact-us.html">Contact Us</a></li>
-                            </ul>
-                        </li>
-                        <li class="scroll-to-section"><a href="#testimonials">Testimonials</a></li> 
-                        <li class="scroll-to-section"><a href="#contact-section">Contact Us</a></li> 
-                    </ul>        
+                        <li class="scroll-to-section"><a href="#testimonials">Testimonials</a></li>
+                        <li class="scroll-to-section"><a href="#contact-section">Contact Us</a></li>
+                        @if (Route::has('login'))
+                            @auth
+                                <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+                            @else
+                                <li><a href="{{ route('login') }}">Log in</a></li>
+                                @if (Route::has('register'))
+                                    <li><a href="{{ route('register') }}">Register</a></li>
+                                @endif
+                            @endauth
+                        @endif
+                    </ul>
                     <a class='menu-trigger'>
                         <span>Menu</span>
                     </a>
